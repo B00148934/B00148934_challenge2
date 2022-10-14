47a7c8a7490ab9adc4aaef919d9262219a8a13f0
B00148934 <B00148934@mytudublin.ie>
2022-10-14 14:17:39 +0100

    initial

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float reduceSpam = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && reduceSpam <=0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            reduceSpam = 1;
+        }
+
+        if (reduceSpam >= 0)
+        {
+            reduceSpam -= Time.deltaTime;
+        }
+    }
+}
